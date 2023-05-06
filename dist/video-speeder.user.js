996a29d556d211c0c67de04ba236d731bf1c3f58
yanggggjie <yanggggjie@gmail.com>
2023-05-06 22:13:03 +0800

    build dist

---
+++ dist/video-speeder.user.js
@@ -0,0 +1,361 @@
+// ==UserScript==
+// @name       video-speeder
+// @namespace  yanggggjie/video-speeder
+// @version    0.0.1
+// @author     monkey
+// @icon       https://github.com/yanggggjie/Video-Speeder/blob/main/assets/icon.svg
+// @match      https://www.bilibili.com/*
+// @match      https://www.youtube.com/watch*
+// @grant      GM_getValue
+// @grant      GM_setValue
+// ==/UserScript==
+
+(t=>{const e=document.createElement("style");e.dataset.source="vite-plugin-monkey",e.textContent=t,document.head.append(e)})(" #showSpeedLabel.svelte-650q51{position:absolute;top:50%;left:50%;z-index:9999;font-size:30px;background-color:wheat;color:#000} ");
+
+(function () {
+  'use strict';
+
+  function noop() {
+  }
+  function run(fn) {
+    return fn();
+  }
+  function blank_object() {
+    return /* @__PURE__ */ Object.create(null);
+  }
+  function run_all(fns) {
+    fns.forEach(run);
+  }
+  function is_function(thing) {
+    return typeof thing === "function";
+  }
+  function safe_not_equal(a, b) {
+    return a != a ? b == b : a !== b || (a && typeof a === "object" || typeof a === "function");
+  }
+  function is_empty(obj) {
+    return Object.keys(obj).length === 0;
+  }
+  function insert(target, node, anchor) {
+    target.insertBefore(node, anchor || null);
+  }
+  function detach(node) {
+    if (node.parentNode) {
+      node.parentNode.removeChild(node);
+    }
+  }
+  function element(name) {
+    return document.createElement(name);
+  }
+  function attr(node, attribute, value) {
+    if (value == null)
+      node.removeAttribute(attribute);
+    else if (node.getAttribute(attribute) !== value)
+      node.setAttribute(attribute, value);
+  }
+  function children(element2) {
+    return Array.from(element2.childNodes);
+  }
+  let current_component;
+  function set_current_component(component) {
+    current_component = component;
+  }
+  function get_current_component() {
+    if (!current_component)
+      throw new Error("Function called outside component initialization");
+    return current_component;
+  }
+  function onMount(fn) {
+    get_current_component().$$.on_mount.push(fn);
+  }
+  const dirty_components = [];
+  const binding_callbacks = [];
+  let render_callbacks = [];
+  const flush_callbacks = [];
+  const resolved_promise = /* @__PURE__ */ Promise.resolve();
+  let update_scheduled = false;
+  function schedule_update() {
+    if (!update_scheduled) {
+      update_scheduled = true;
+      resolved_promise.then(flush);
+    }
+  }
+  function add_render_callback(fn) {
+    render_callbacks.push(fn);
+  }
+  const seen_callbacks = /* @__PURE__ */ new Set();
+  let flushidx = 0;
+  function flush() {
+    if (flushidx !== 0) {
+      return;
+    }
+    const saved_component = current_component;
+    do {
+      try {
+        while (flushidx < dirty_components.length) {
+          const component = dirty_components[flushidx];
+          flushidx++;
+          set_current_component(component);
+          update(component.$$);
+        }
+      } catch (e) {
+        dirty_components.length = 0;
+        flushidx = 0;
+        throw e;
+      }
+      set_current_component(null);
+      dirty_components.length = 0;
+      flushidx = 0;
+      while (binding_callbacks.length)
+        binding_callbacks.pop()();
+      for (let i = 0; i < render_callbacks.length; i += 1) {
+        const callback = render_callbacks[i];
+        if (!seen_callbacks.has(callback)) {
+          seen_callbacks.add(callback);
+          callback();
+        }
+      }
+      render_callbacks.length = 0;
+    } while (dirty_components.length);
+    while (flush_callbacks.length) {
+      flush_callbacks.pop()();
+    }
+    update_scheduled = false;
+    seen_callbacks.clear();
+    set_current_component(saved_component);
+  }
+  function update($$) {
+    if ($$.fragment !== null) {
+      $$.update();
+      run_all($$.before_update);
+      const dirty = $$.dirty;
+      $$.dirty = [-1];
+      $$.fragment && $$.fragment.p($$.ctx, dirty);
+      $$.after_update.forEach(add_render_callback);
+    }
+  }
+  function flush_render_callbacks(fns) {
+    const filtered = [];
+    const targets = [];
+    render_callbacks.forEach((c) => fns.indexOf(c) === -1 ? filtered.push(c) : targets.push(c));
+    targets.forEach((c) => c());
+    render_callbacks = filtered;
+  }
+  const outroing = /* @__PURE__ */ new Set();
+  function transition_in(block, local) {
+    if (block && block.i) {
+      outroing.delete(block);
+      block.i(local);
+    }
+  }
+  function mount_component(component, target, anchor, customElement) {
+    const { fragment, after_update } = component.$$;
+    fragment && fragment.m(target, anchor);
+    if (!customElement) {
+      add_render_callback(() => {
+        const new_on_destroy = component.$$.on_mount.map(run).filter(is_function);
+        if (component.$$.on_destroy) {
+          component.$$.on_destroy.push(...new_on_destroy);
+        } else {
+          run_all(new_on_destroy);
+        }
+        component.$$.on_mount = [];
+      });
+    }
+    after_update.forEach(add_render_callback);
+  }
+  function destroy_component(component, detaching) {
+    const $$ = component.$$;
+    if ($$.fragment !== null) {
+      flush_render_callbacks($$.after_update);
+      run_all($$.on_destroy);
+      $$.fragment && $$.fragment.d(detaching);
+      $$.on_destroy = $$.fragment = null;
+      $$.ctx = [];
+    }
+  }
+  function make_dirty(component, i) {
+    if (component.$$.dirty[0] === -1) {
+      dirty_components.push(component);
+      schedule_update();
+      component.$$.dirty.fill(0);
+    }
+    component.$$.dirty[i / 31 | 0] |= 1 << i % 31;
+  }
+  function init(component, options, instance2, create_fragment2, not_equal, props, append_styles, dirty = [-1]) {
+    const parent_component = current_component;
+    set_current_component(component);
+    const $$ = component.$$ = {
+      fragment: null,
+      ctx: [],
+      // state
+      props,
+      update: noop,
+      not_equal,
+      bound: blank_object(),
+      // lifecycle
+      on_mount: [],
+      on_destroy: [],
+      on_disconnect: [],
+      before_update: [],
+      after_update: [],
+      context: new Map(options.context || (parent_component ? parent_component.$$.context : [])),
+      // everything else
+      callbacks: blank_object(),
+      dirty,
+      skip_bound: false,
+      root: options.target || parent_component.$$.root
+    };
+    append_styles && append_styles($$.root);
+    let ready = false;
+    $$.ctx = instance2 ? instance2(component, options.props || {}, (i, ret, ...rest) => {
+      const value = rest.length ? rest[0] : ret;
+      if ($$.ctx && not_equal($$.ctx[i], $$.ctx[i] = value)) {
+        if (!$$.skip_bound && $$.bound[i])
+          $$.bound[i](value);
+        if (ready)
+          make_dirty(component, i);
+      }
+      return ret;
+    }) : [];
+    $$.update();
+    ready = true;
+    run_all($$.before_update);
+    $$.fragment = create_fragment2 ? create_fragment2($$.ctx) : false;
+    if (options.target) {
+      if (options.hydrate) {
+        const nodes = children(options.target);
+        $$.fragment && $$.fragment.l(nodes);
+        nodes.forEach(detach);
+      } else {
+        $$.fragment && $$.fragment.c();
+      }
+      if (options.intro)
+        transition_in(component.$$.fragment);
+      mount_component(component, options.target, options.anchor, options.customElement);
+      flush();
+    }
+    set_current_component(parent_component);
+  }
+  class SvelteComponent {
+    $destroy() {
+      destroy_component(this, 1);
+      this.$destroy = noop;
+    }
+    $on(type, callback) {
+      if (!is_function(callback)) {
+        return noop;
+      }
+      const callbacks = this.$$.callbacks[type] || (this.$$.callbacks[type] = []);
+      callbacks.push(callback);
+      return () => {
+        const index = callbacks.indexOf(callback);
+        if (index !== -1)
+          callbacks.splice(index, 1);
+      };
+    }
+    $set($$props) {
+      if (this.$$set && !is_empty($$props)) {
+        this.$$.skip_bound = true;
+        this.$$set($$props);
+        this.$$.skip_bound = false;
+      }
+    }
+  }
+  var _GM_getValue = /* @__PURE__ */ (() => typeof GM_getValue != "undefined" ? GM_getValue : void 0)();
+  var _GM_setValue = /* @__PURE__ */ (() => typeof GM_setValue != "undefined" ? GM_setValue : void 0)();
+  function create_fragment(ctx) {
+    let div;
+    return {
+      c() {
+        div = element("div");
+        attr(div, "id", "showSpeedLabel");
+        attr(div, "class", "svelte-650q51");
+      },
+      m(target, anchor) {
+        insert(target, div, anchor);
+        ctx[1](div);
+      },
+      p: noop,
+      i: noop,
+      o: noop,
+      d(detaching) {
+        if (detaching)
+          detach(div);
+        ctx[1](null);
+      }
+    };
+  }
+  function instance($$self, $$props, $$invalidate) {
+    let globalSpeed = _GM_getValue("globalSpeed") ?? 1;
+    let showSpeedLabel;
+    onMount(() => {
+      const video = document.querySelector("video");
+      const parent = video.parentElement;
+      let timer;
+      parent.style.position = "relative";
+      parent.appendChild(showSpeedLabel);
+      video.playbackRate = globalSpeed;
+      $$invalidate(0, showSpeedLabel.innerText = video.playbackRate, showSpeedLabel);
+      showSpeed();
+      document.addEventListener("keydown", function(event) {
+        const key = event.key.toLowerCase();
+        switch (key) {
+          case "c": {
+            video.playbackRate += 0.25;
+            $$invalidate(0, showSpeedLabel.innerText = video.playbackRate, showSpeedLabel);
+            showSpeed();
+            break;
+          }
+          case "x": {
+            if (video.playbackRate > 0.25) {
+              video.playbackRate -= 0.25;
+            }
+            $$invalidate(0, showSpeedLabel.innerText = video.playbackRate, showSpeedLabel);
+            showSpeed();
+            break;
+          }
+          case "z": {
+            video.playbackRate = 1;
+            $$invalidate(0, showSpeedLabel.innerText = video.playbackRate, showSpeedLabel);
+            showSpeed();
+            break;
+          }
+        }
+        _GM_setValue("globalSpeed", video.playbackRate, true);
+      });
+      function showSpeed() {
+        $$invalidate(0, showSpeedLabel.style.visibility = "visible", showSpeedLabel);
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(
+          () => {
+            $$invalidate(0, showSpeedLabel.style.visibility = "hidden", showSpeedLabel);
+          },
+          700
+        );
+      }
+    });
+    function div_binding($$value) {
+      binding_callbacks[$$value ? "unshift" : "push"](() => {
+        showSpeedLabel = $$value;
+        $$invalidate(0, showSpeedLabel);
+      });
+    }
+    return [showSpeedLabel, div_binding];
+  }
+  class App extends SvelteComponent {
+    constructor(options) {
+      super();
+      init(this, options, instance, create_fragment, safe_not_equal, {});
+    }
+  }
+  new App({
+    target: (() => {
+      const app2 = document.createElement("div");
+      document.body.append(app2);
+      return app2;
+    })()
+  });
+
+})();
